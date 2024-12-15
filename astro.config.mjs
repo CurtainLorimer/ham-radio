c29247b2ef2c3631c4b78e68c94e3ee894ac06cc
lwu877 <67766822+lwu877@users.noreply.github.com>
2024-12-15 15:37:29 -0500

    Complete branding change
    Got rid of template info, removed Vercel integration, added HAM-Radio branding

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,9 @@ import db from '@astrojs/db';
 import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  adapter: vercel({
-    imageService: true,
-    speedInsights: {
-      enabled: true
-    },
-    webAnalytics: {
-      enabled: true
-    }
-  }),
   site: 'https://ham.curtainlorimer.com',
   integrations: [
     db(),
@@ -28,12 +17,4 @@ export default defineConfig({
     })
   ],
   // These were specific redirects we needed for our podcast, if you do not have any routes to redirect, you can safely remove this.
-  redirects: {
-    '/hot-takes-tan-stack-and-open-source-with-tanner-linsley':
-      '/hot-takes-tanstack-and-open-source-with-tanner-linsley',
-    '/creating-code-pen-tackling-tailwind-and-keeping-it-simple-with-chris-coyier':
-      'creating-codepen-tackling-tailwind-and-keeping-it-simple-with-chris-coyier',
-    '/coding-languages-ai-and-the-evolution-of-game-development-with-phillip-winston':
-      '/coding-languages-ai-and-the-evolution-of-game-development-with-philip-winston'
-  }
 });
--- starpod.config.ts
+++ starpod.config.ts
@@ -3,32 +3,30 @@ import { defineStarpodConfig } from 'src/utils/config';
 export default defineStarpodConfig({
   blurb: 'A creative writing prompt, fun facts, and history in a minute (maybe two)',
   description:
-    'Lex Wu and Jayden Alip host a weekly podcast featuring a creative writing prompt, fun facts, and history in a minute or two. Aimed for people who want to learn a few fun facts while enjoying their weekend writing, or parents who just want their kids to do something on the weekends.',
+    'Lex Wu and Vian Vera host a weekly podcast featuring a creative writing prompt, fun facts, and history in a minute or two. Aimed for people who want to learn a few fun facts while enjoying their weekend writing, or parents who just want their kids to do something on the weekends.',
   hosts: [
     {
       name: 'Lex Wu',
       bio: 'A musician that does way too much stuff.',
-      img: 'robbiethewagner.jpg',
+      img: 'lexwu.png',
       github: 'https://github.com/lwu877',
       twitter: 'https://twitter.com/lwu877',
       website: 'https://lexwu.com'
     },
     {
-      name: 'Jayden Alip',
-      bio: 'Third of his name, user of gifs, hater of ESM.',
-      img: 'chuckcarpenter.jpg',
-      github: 'https://github.com/chuckcarpenter',
-      twitter: 'https://twitter.com/CharlesWthe3rd'
+      name: 'Vian Vera',
+      bio: 'An artist and writer that similarly does too much.',
+      img: 'vianvera.png',
     }
   ],
   platforms: {
     apple:
-      'https://podcasts.apple.com/us/podcast/whiskey-web-and-whatnot/id1552776603?uo=4?mt=2&ls=1',
-    appleIdNumber: '1552776603',
-    overcast: 'https://overcast.fm/itunes1552776603',
-    pocketCasts: 'https://pca.st/bezzctzj',
-    spotify: 'https://open.spotify.com/show/19jiuHAqzeKnkleQUpZxDf',
-    youtube: 'https://www.youtube.com/@WhiskeyWebAndWhatnot/'
+      'https://podcasts.apple.com/us/podcast/heres-a-minute/id1785493881',
+    appleIdNumber: '1785493881',
+    overcast: 'https://overcast.fm/itunes1785493881',
+    pocketCasts: 'https://pca.st/htmr8etd',
+    spotify: 'https://open.spotify.com/show/1Xmaek9r2UmdnwTZZRCFVG?si=339286518efc4173',
+    youtube: 'https://www.youtube.com/playlist?list=PLc9AhlksfpMq4qPg54pNHjr0Y3KyaW3xX'
   },
-  rssFeed: 'https://anchor.fm/s/e329dea0/podcast/rss'
+  rssFeed: 'https://anchor.fm/s/e6b46a04/podcast/rss'
 });
